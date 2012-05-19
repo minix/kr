59827f148619301504132d81df61993ba84bbe78
minix <mgtuiw@gmail.com>
2012-05-19 16:52:23 +0800

    show image to index

--- app/controllers/present_controller.rb
+++ app/controllers/present_controller.rb
@@ -4,19 +4,6 @@ class PresentController < ApplicationController
 			@user = User.find_by_id(session[:user])
 		end
 		@photo = Photo.find(:all)
-#		send_data(@show,
-#			filename: @show.name,
-#			type: @show.content_type,
-#			disposition: "inline"
-#		)
 	end
-
-	def show
-		@photo = Photo.find(params[:id])
-	end
-	#def show_image
-	#	show = Photo.find(params[:id])
-	#	send_data @photo.large, :type => 'image/png', :disposition => 'inline'
-	#end
 	
 end
--- app/models/present.rb
+++ app/models/present.rb
@@ -1,5 +1,3 @@
 class Present < ActiveRecord::Base
 	has_one :photos
-	validates_format_of :content_type, 
-		with: /^image/
 end
