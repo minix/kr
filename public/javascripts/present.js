1af2f320b9ae71de5e914e21afb845815f9ba1d1
minix <mgtuiw@gmail.com>
2012-05-23 16:10:38 +0800

    add js

--- public/javascripts/present.js
+++ public/javascripts/present.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+	$('.item').hover(function() {
+		$(this).find('div.caption').stop(false,true).fadeIn(300);
+	},
+	function() {
+		$(this).find('div.caption').stop(false,true).fadeOut(400);
+	});
+});
