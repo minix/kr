1d444c6a4bbf6516b04efd8d300e8c4fdfd38f7f
minix <mgtuiw@gmail.com>
2012-05-28 17:40:08 +0800

    add talk

--- app/controllers/talk_controller.rb
+++ app/controllers/talk_controller.rb
@@ -3,5 +3,28 @@ class TalkController < ApplicationController
 		if session[:user]
 			@user = User.find_by_id(session[:user])
 		end
+		@t = Talk.find(:all)
   end
+
+	def talk
+		if session[:user]
+			@user = User.find_by_id(session[:user])
+		end
+		@talk = Talk.new(params[:talk])
+		@talk.user_name = @user.name 
+	 	if @talk.save
+	 		flash[:notice] = "Post Success! "
+			redirect_to controller: "talk", action: "index"
+	 	end
+	end
+
+	def destroy
+		@talk = Talk.find(params[:id])
+		@talk.destroy
+		redirect_to controller: "talk", action: "index"
+	end
+
+	def modify
+		 @talk = Talk.find(params[:id])
+	end
 end
