4e9ebf37b5ccc151fdb6172a5cdbb5358280bb80
minix <mgtuiw@gmail.com>
2012-07-04 12:31:59 +0800

    change css

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,9 +5,11 @@ class UserController < ApplicationController
 			if @user.save
 				session[:user] = User.authenticate(@user.name, @user.password)
 				flash[:notice] = "Signup success"
+				flash[:color] = "valid"
 				redirect_to controller: "present", action: "index"
 			else
 				flash[:warning] = "Signup Unsuccessful"
+				flash[:color] = "invalid"
 			end
 		end
   end
@@ -16,10 +18,12 @@ class UserController < ApplicationController
 		if request.post?
 			if session[:user] = User.authenticate(params[:user][:name], params[:user][:passwd])
 				flash[:notice] = "login successful"
-				#redirect_to_stored
+				flash[:color] = "valid"
 				redirect_to controller: "present", action: "index"
 			else
-				flash[:warning] = "login unsuccessful"
+				flash[:warning] = "Invalid Username or Password"
+				flash[:color] = "invalid"
+				render "login"
 			end
 		end
   end
